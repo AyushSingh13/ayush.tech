bdb8f01c5db3ac26104bd49a43cd1cbbbb3fe892
Ayush Singh <ayush.vijay@gmail.com>
2018-07-01 14:48:50 +0100

    replaced CSS Modules styles with Styled-Components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,44 @@
 import React from "react"
 import Link from 'gatsby-link'
-import styles from './styles.module.css'
+import styled from 'styled-components';
 import ContactBar from '../components/ContactBar'
 import headshot from '../static/headshot.jpg'
 
-const pageStyle = {
-    minWidth: `100vw`,
-    minHeight: `100vh`,
-    display: `flex`,
-    alignItems: `center`,
-    justifyContent: `center`,
-}
+const Page = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const nameStyle = {
-    marginBottom: `10px`
-}
+const NameHeader = styled.h1`
+  margin-bottom: 10px;
+`;
 
-const headshotStyle = {
-    borderRadius: `100%`,
-    height: `7rem`,
-    width: `7rem`
-}
+const RedTextSpan = styled.span`
+  color: #e74c3c;
+`;
+
+const Image = styled.img`
+  border-radius: 100%;
+  height: ${props => props.size};
+  width: ${props => props.size};
+`;
+
+const ShortBio = styled.div`
+  font-family: 'Lato', 'Helvetica Neue';
+  font-weight: 100;
+  font-size: 19px;
+`;
 
 export default () => (
-    <div style={pageStyle}>
+    <Page>
         <main style={{ display: `flex`, flexDirection: `column`, alignItems: `center` }}>
-            <img style={headshotStyle} src={headshot} />
-            <h1 style={nameStyle}>ayush <span id={styles.singh}>singh</span></h1>
-            <span id={styles.shortBio}>software engineer | london | united kingdom</span>
+            <Image size="7em" src={headshot} />
+            <NameHeader>ayush <RedTextSpan>singh</RedTextSpan></NameHeader>
+            <ShortBio>software engineer | london | united kingdom</ShortBio>
             <ContactBar />
         </main>
-    </div>
-)
+    </Page>
+);
