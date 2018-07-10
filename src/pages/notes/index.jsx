3559b9f0b085d51631e340462af6f3e4aa0a7122
Ayush Singh <ayush.vijay@gmail.com>
2018-07-10 03:06:52 +0100

    changed 'blog' section to 'notes'

---
+++ src/pages/notes/index.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Link from "gatsby-link";
+import styled from "styled-components";
+
+// const Page = styled.div`
+//   min-width: 100vw;
+//   padding: 2rem;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+
+//   @media (max-width: 768px) {
+//     padding: 1rem;
+//   }
+// `;
+
+const PostList = styled.div`
+  width: 50%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const PostLink = styled(Link)`
+  display: flex;
+  flex-direction: column;
+
+  text-decoration: none;
+  color: inherit;
+
+  cursor: pointer;
+  padding: 0.5rem;
+  transition: 0.1s all;
+
+  &:hover {
+    background-color: #f5f6fa;
+  }
+`;
+
+const PostCount = styled.h3`
+  padding: 0.5rem;
+`;
+
+const PostLinkHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0;
+`;
+
+const PostTitle = styled.h1`
+  margin: 0;
+`;
+
+const PostDate = styled.h5`
+  font-weight: 100;
+  maring: 0;
+  opacity: 0.7;
+`;
+
+const PostExcerpt = styled.p`
+  margin: 0;
+`;
+
+export default ({ data }) => (
+  <PostList>
+    <PostCount>{data.allMarkdownRemark.totalCount} Posts</PostCount>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <PostLink key={node.id} to={node.fields.slug}>
+        <PostLinkHeader>
+          <PostTitle>{node.frontmatter.title}</PostTitle>
+          <PostDate>{node.frontmatter.date}</PostDate>
+        </PostLinkHeader>
+        <PostExcerpt>{node.excerpt}</PostExcerpt>
+      </PostLink>
+    ))}
+  </PostList>
+);
+
+export const query = graphql`
+  query PostQuery {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          fields {
+            slug
+          }
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
